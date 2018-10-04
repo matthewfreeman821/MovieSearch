430866fa39e082da76034b6cd6fde69a556aabcf
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-04 13:36:29 -0400

    Updated js file to show individual movie details, not currently working, session storage not storing as key value pairs

--- js/script.js
+++ js/script.js
@@ -18,11 +18,56 @@ function getMovies(searchText) {
                         <div class="well text-center">
                             <img src="${movie.Poster}">
                             <h5>${movie.Title}</h5>
-                            <a onclick="movieSelected('${movie.imdbID}') class="btn btn-primary" href="#">Movie Details</a>
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                         </div>
                     </div>
                 `;
             });
             $('#movies').html(output);
+        });
+}
+
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);//Issue is somewhere here, not storing as key value pair, only storing id in sessionStorage
+    window.location = 'movie.html';
+    return true;
+};
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=4e37285d&i=' + movieId)
+        .then((response) => {
+            console.log(response);
+            let movie = response.data;
+            let output = `
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${movie.poster}" class="thumbnail">
+                    </div>
+                    <div class="col-md-8">
+                        <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                            <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                            <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                            <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                            <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                            <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <h3>Plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a href="index.html" class="btn btn-default">Back to Search</a>
+                    </div>
+                </div>
+
+            `;
+            $('#movie').html(output);
         });
 }
